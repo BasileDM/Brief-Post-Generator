63b134d46ebd9b32f9c260bd2edae761c36b7f1d
BasileDM <xnosx@hotmail.fr>
2025-01-27 15:54:23 +0100

    Define method to split slogan into multiple lines

--- src/components/ResultCanvas.vue
+++ src/components/ResultCanvas.vue
@@ -67,6 +67,25 @@ const getTemplateVariables = (templateType: string): TemplateVariables => {
   }
 }
 
+function getSloganLines(ctx: CanvasRenderingContext2D, slogan: string, maxWidth = 400) {
+  var words = slogan.split(" ");
+  var lines = [];
+  var currentLine = words[0];
+
+  for (var i = 1; i < words.length; i++) {
+    var word = words[i];
+    var width = ctx.measureText(currentLine + " " + word).width;
+    if (width < maxWidth) {
+      currentLine += " " + word;
+    } else {
+      lines.push(currentLine);
+      currentLine = word;
+    }
+  }
+  lines.push(currentLine);
+  return lines;
+}
+
 watch([() => title, () => slogan, () => templateType], drawCanvas);
 
 onMounted(() => {
