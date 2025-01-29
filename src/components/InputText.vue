2b9b27ebfdcd3f745a06475cc31a47431d4d12d7
Laurent <laurent.roches.simplon@gmail.com>
2025-01-29 11:52:09 +0100

    Fixe : input number from 0 to 10

--- src/components/InputText.vue
+++ src/components/InputText.vue
@@ -1,10 +1,20 @@
 <script setup lang="ts">
 defineProps<{ title_input: string, type_input: string }>()
+const verify = (event: Event) => {
+    const inputElement = event.target as HTMLInputElement;
+    let value = inputElement.value.trim();
+    value = value.replace(/[^0-9]/g, '');
+    if(Number(value) > Number(inputElement.max)) {
+        value = inputElement.max;
+    }
+    inputElement.value = value;
+};
 </script>
 
 <template>
     <label for="" class="title">{{ title_input }}</label>
-    <input :type="type_input" :name="title_input" class="field">
+    <input v-if="type_input == 'number'" min="0" step="1" max="10" :type="type_input"  :name="title_input" class="field" @input="verify">
+    <input v-else :type="type_input"  :name="title_input" class="field">
 </template>
 
 <style scoped>
