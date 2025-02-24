fd69d9da3d39ca9db80d30b2febddd7bf66cde4f
Laurent <laurent.roches.simplon@gmail.com>
2025-02-24 16:49:39 +0100

    Create: size gestion for title and slogan

--- src/components/ResultCanvas.vue
+++ src/components/ResultCanvas.vue
@@ -2,14 +2,16 @@
 import { onMounted, watch, ref } from 'vue'
 import type { TemplateVariables } from '../interfaces/TemplateVariables';
 
-const { templateType, title, titleColor, titleFont, slogan, sloganColor, sloganFont } = defineProps({
+const { templateType, title, titleColor, titleFont, titleSize, slogan, sloganColor, sloganFont, sloganSize } = defineProps({
   templateType: String,
   title: String,
   titleColor: String,
   titleFont: String,
   slogan: String,
   sloganColor: String,
-  sloganFont: String
+  sloganFont: String,
+  titleSize: Number, 
+  sloganSize: Number 
 })
 
 const imageRef = ref<HTMLImageElement | null>(null)
@@ -47,37 +49,38 @@ const drawCanvas = async () => {
   ctx.drawImage(image, 0, 0);
 
   // Draw the title
-  ctx.font = `40px ${titleFont || 'serif'}`;
+  ctx.font = `${titleSize || 40}px ${titleFont || 'serif'}`;
+  
   const titleLines = getLines(ctx, title as string, 380);
+  if(!titleSize) {
+    throw new Error();
+  }
+  const titleLineHeight = titleSize * 1.2;
 
   ctx.fillStyle = 'rgba(0, 0, 0, 0.8)';
-  ctx.fillRect(
-    0,
-    titlePosition.y - 33,
-    402,
-    40 * titleLines.length
-  );
+  ctx.fillRect(0, titlePosition.y - titleSize * 0.8, 402, titleLineHeight * titleLines.length);
+
+  ctx.fillStyle = titleColor || 'white';
   for (let i = 0; i < titleLines.length; i++) {
-    ctx.fillStyle = titleColor || 'white';
-    ctx.fillText(titleLines[i], titlePosition.x, titlePosition.y + i * 40);
+    ctx.fillText(titleLines[i], titlePosition.x, titlePosition.y + i * titleLineHeight);
   }
 
   // Draw the slogan
-  ctx.font = `20px ${sloganFont || 'serif'}`;
+  ctx.font = `${sloganSize || 20}px ${sloganFont || 'serif'}`;
   const sloganLines = getLines(ctx, slogan as string, 380);
+  if(!sloganSize) {
+    throw new Error();
+  }
+  const sloganLineHeight = sloganSize * 1.2;
 
-  const sloganStartPosition = templateType == 'template_2' ? sloganPosition.y : sloganPosition.y + titleLines.length * 40;
+  const sloganStartPosition = templateType == 'template_2' ? sloganPosition.y : sloganPosition.y + titleLines.length * titleLineHeight;
+  
   for (let i = 0; i < sloganLines.length; i++) {
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.8)'
-    ctx.fillRect(
-      0,
-      sloganStartPosition - 21 + i * 30,
-      402,
-      30
-    )
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.8)';
+    ctx.fillRect(0, sloganStartPosition - sloganSize * 0.8 + i * sloganLineHeight, 402, sloganLineHeight);
     ctx.fillStyle = sloganColor || 'white';
-    ctx.fillText(sloganLines[i], sloganPosition.x, sloganStartPosition + i * 30);
-  };
+    ctx.fillText(sloganLines[i], sloganPosition.x, sloganStartPosition + i * sloganLineHeight);
+  }
 }
 
 const getTemplateVariables = (templateType: string): TemplateVariables => {
@@ -121,7 +124,7 @@ function getLines(ctx: CanvasRenderingContext2D, slogan: string, maxWidth = 400)
 
 defineExpose({ loadImage, drawCanvas });
 
-watch([() => title, () => titleColor, () => titleFont, () => slogan, () => sloganColor, () => sloganFont, () => templateType, () => imageRef], drawCanvas);
+watch([() => title, () => titleColor, () => titleFont, () => titleSize, () => slogan, () => sloganColor, () => sloganFont, () => sloganSize, () => templateType, () => imageRef], drawCanvas);
 
 onMounted(async () => {
   imageRef.value = await loadImage();
